ce97e9342b67e7bdb88e39fe275e90c315abd741
churchillianokoth <churchill.okoth@strathmore.edu>
2025-08-07 19:43:53 +0300

    First commit

--- test.java
+++ test.java
@@ -12,13 +12,22 @@ class Test
 		int science =85;
 		int sst = 90;
 		
+		String[] subjects = {"maths","english","kiswahili","science","sst"};
+		int[] scores = {maths,english,kiswahili,science,sst};
+		
 		int total = maths+english+kiswahili+science+sst;
 		
 		//System.out.println("The total is ="+total);
 		//System.out.println("The grade is = "+getGrade(total));
 		//test our modularized code
 		printMyData("The total of test is",total);
-		printMyData("The Grade is ",'T');
+		printMyData("The Grade is ",getGrade(total));
+		printMyData(getHighestScores(subjects, scores));
+		printMyData(getLeastScores(subjects, scores));
+		int[] sumProduct = calculateSumAndProduct(scores);
+		 printMyData("Sum of scores is " + sumProduct[0]);
+        printMyData("Product of scores is " + sumProduct[1]);
+		
 		
 	}
 	
@@ -32,10 +41,60 @@ class Test
 		
 		return grade;
 	}
-	
-	public static void printMyData(String statement,int data)
+	public static String getHighestScores(String[] subjects, int[] scores)
+	{
+		int maxIndex = 0;
+         for(int i = 1; i < scores.length; i++)
+		 {
+			 if(scores[i] > scores[maxIndex])
+			 {
+				 maxIndex = i;
+			 }
+		 }	
+return subjects[maxIndex]+"is the best score"+scores[maxIndex];		 
+	}
+	public static String getLeastScores(String[] subjects, int[] scores)
+	{
+		int minIndex = 0;
+		for(int i = 1; i < scores.length; i++)
+		{
+		if(scores[i] < scores[minIndex])
+		{
+			minIndex = i;
+		}
+		}
+		return subjects[minIndex]+"the least score is"+scores[minIndex];
+	}
+	public static int[] calculateSumAndProduct(int[]scores)
+	{
+		int sum = 0;
+		int product = 1;
+		for( int score : scores)
+		{
+			sum += score;
+		product *= score;
+		}
+		
+		return new int[] {sum,product};
+	}
+
+	public static void printMyData(String statement,int data )
 	{
 		
+		System.out.println(statement+ data );
+	}
+	
+	public static void printMyData(String statement, char data)
+	{
 		System.out.println(statement+data);
 	}
-}
+	public static void printMyData(String message)
+	{
+		System.out.println(message);
+	}
+	public static void printMyData(int data)
+	{
+		System.out.println(data);
+	}
+	
+}  
